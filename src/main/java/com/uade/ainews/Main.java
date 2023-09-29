0ba9c7eea73170f8038f3fb0b827d8c8e63c02e9
Gianfranco Caneva <gfocaneva@gmail.com>
2023-09-29 03:08:34 -0300

    Implement docker-compose logic

--- src/main/java/com/uade/ainews/Main.java
+++ src/main/java/com/uade/ainews/Main.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
 
+    //COMMAND TO RUN THE APPLICATION THROUGH CLI ./mvnw spring-boot:run
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
